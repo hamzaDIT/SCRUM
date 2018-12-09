32427534e52d57db7a58ff4cc71dfa26213e9f33
Alina Kazakova <alinakazakova@live.ie>
2018-12-09 14:28:04 +0000

    Test

---
+++ SCRUMWorkshop/src/workshop/Test.java
@@ -0,0 +1,13 @@
+package workshop;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 1. Alina made this change
+		// 2. Hamza
+		// 3. Daniel
+	}
+
+}
